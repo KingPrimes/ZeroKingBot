278ff1e6c83efe9e61d04d5b111e152026bbbb8b
KingPrimes <Sakitama_q@163.com>
2022-08-20 15:02:30 +0800

    修复查询 紫卡倾向变动 的Bug

--- zkb-warframe/src/main/java/com/zkb/bot/warframe/controller/forums/RivenDispositionUpdatesController.java
+++ zkb-warframe/src/main/java/com/zkb/bot/warframe/controller/forums/RivenDispositionUpdatesController.java
@@ -8,12 +8,13 @@ import com.zkb.bot.warframe.utils.forums.RivenDispositionUpdatesImage;
 import com.zkb.common.core.redis.RedisCache;
 import com.zkb.common.utils.file.FileUtils;
 import com.zkb.common.utils.spring.SpringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
 import java.util.List;
@@ -22,12 +23,14 @@ import java.util.List;
 @RequestMapping("/warframe/forums/riven")
 public class RivenDispositionUpdatesController {
 
+    private static final Logger log = LoggerFactory.getLogger(RivenDispositionUpdatesController.class);
+
     @Autowired
     RedisCache redisCache;
 
 
     @GetMapping(value = "/{uuid}/getNewsImage")
-    public void getRivenDisUpdatesImage(HttpServletResponse response, HttpServletRequest request) throws Exception {
+    public void getRivenDisUpdatesImage(HttpServletResponse response) {
         response.setHeader("Content-Type", "image/png");
         try {
             //获取文件流返回
@@ -37,7 +40,7 @@ public class RivenDispositionUpdatesController {
             List<WarframeRivenTrend> trends;
             try {
                 trends = redisCache.getCacheList("renew-riven-disposition");
-                if (trends.size() == 0) {
+                if (trends.size() <= 0) {
                     new RivenDispositionUpdatesTask().renewRivenDisposition();
                     trends = redisCache.getCacheList("renew-riven-disposition");
                 }
@@ -50,7 +53,8 @@ public class RivenDispositionUpdatesController {
                 RivenDispositionUpdatesImage.getImage(image);
                 //返回文件流
                 response.getOutputStream().write(FileUtils.toByteArray3("./renew-riven-disposition.jpg"));
-            } catch (Exception exception) {
+            } catch (Exception ex) {
+                ex.printStackTrace();
                 //如果Redis中没有缓存则获取缓存
                 new RivenDispositionUpdatesTask().renewRivenDisposition();
             }
