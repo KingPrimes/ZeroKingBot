9c8bec9b67d0adea187740b7f483ad5212299cdf
KingPrimes <15066650082@163.com>
2023-02-20 22:46:08 +0800

    问答AI更改触发方式。改为艾特Bot触发

--- zkb-directives/src/main/java/com/zkb/bot/plugin/aiml/IssueReplyPlugin.java
+++ zkb-directives/src/main/java/com/zkb/bot/plugin/aiml/IssueReplyPlugin.java
@@ -6,9 +6,10 @@ import com.mikuac.shiro.core.Bot;
 import com.mikuac.shiro.dto.event.message.GroupMessageEvent;
 import com.zkb.bot.aiml.domain.IssueReply;
 import com.zkb.bot.aiml.service.IssueReplyService;
-import com.zkb.bot.aiml.utils.OtherUtils;
 import com.zkb.bot.aiml.utils.TeachingUtils;
 import com.zkb.bot.enums.FunctionEnums;
+import com.zkb.bot.utils.CqMatcher;
+import com.zkb.bot.utils.CqParse;
 import com.zkb.bot.utils.Msg;
 import com.zkb.bot.utils.SelectGroupFunctionOnOff;
 import com.zkb.common.utils.html.EscapeUtil;
@@ -27,31 +28,51 @@ public class IssueReplyPlugin {
     public int onGroupMessage(@NotNull Bot bot, @NotNull GroupMessageEvent event) {
 
         if (SelectGroupFunctionOnOff.getGroupFunctionOnOff(event.getGroupId(), FunctionEnums.FUNCTION_AI)) {
-            //触发概率 不准确
-            if (OtherUtils.randomEx(30)) {
-                Msg msg = new Msg();
-                IssueReply issueReply = SpringUtils.getBean(TeachingUtils.class).getIssue(event.getRawMessage());
-                if(issueReply!=null){
-                    issueReply = service.selectIssueReplyByMsgIssue(issueReply);
-                }
-                if (issueReply != null) {
-                    if (issueReply.getMsgReplyImage() != null) {
-                        String[] urls = issueReply.getMsgReplyImage().replaceAll("\\[", "").replaceAll("]", "").split(",");
-                        for (String url : urls) {
-                            msg.img(url.trim());
+            //@Bot触发
+            if (CqMatcher.isCqAt(event.getRawMessage())) {
+                for (Long aLong : CqParse.build(event.getMessage()).getCqAt()) {
+                    if(aLong.equals(bot.getSelfId())){
+                        Msg msg = new Msg();
+                        IssueReply issueReply = SpringUtils.getBean(TeachingUtils.class).getIssue(event.getRawMessage());
+                        if(issueReply!=null){
+                            issueReply = service.selectIssueReplyByMsgIssue(issueReply);
+                        }
+                        if (issueReply == null){
+                            bot.sendGroupMsg(event.getGroupId(),bot.getGroupMemberInfo(event.getGroupId(),bot.getSelfId(),false).getData().getNickname()+"，不明白你在讲什么呢！",false);
+                        }
+
+                        if (issueReply != null) {
+                            if (!issueReply.getMsgReplyImage().isEmpty()) {
+                                String[] urls = issueReply.getMsgReplyImage().replaceAll("\\[", "").replaceAll("]", "").split(",");
+                                for (String url : urls) {
+                                    msg.img(url.trim());
+                                }
+                            }
+                            if (!issueReply.getMsgReplyFace().isEmpty()) {
+                                String[] ids = issueReply.getMsgReplyFace().replaceAll("\\[", "").replaceAll("]", "").trim().split(",");
+                                for (String id : ids) {
+                                    msg.face(Integer.parseInt(id));
+                                }
+                            }
+                            if (!issueReply.getMsgReply().isEmpty()) {
+                                String[] split = EscapeUtil.unescape(issueReply.getMsgReply()
+                                        .replace("{name}", bot.getGroupMemberInfo(event.getGroupId(), event.getUserId(), true).getData().getNickname())
+                                        .replace("{me}", bot.getGroupMemberInfo(event.getGroupId(), bot.getSelfId(), true).getData().getNickname())
+                                ).split("\\{segment}");
+
+                                if (split.length>1){
+                                    for (String s : split) {
+                                        bot.sendGroupMsg(event.getGroupId(),s,false);
+                                    }
+                                    return 1;
+                                }else {
+                                    msg.text(split[0]);
+                                }
+                            }
+                            msg.sendToGroup(bot, event);
+                            return 1;
                         }
                     }
-                    if (issueReply.getMsgReplyFace() != null) {
-                        String[] ids = issueReply.getMsgReplyFace().replaceAll("\\[", "").replaceAll("]", "").trim().split(",");
-                        for (String id : ids) {
-                            msg.face(Integer.parseInt(id));
-                        }
-                    }
-                    if (issueReply.getMsgReply() != null) {
-                        msg.text(EscapeUtil.unescape(issueReply.getMsgReply()));
-                    }
-                    msg.sendToGroup(bot, event);
-                    return 1;
                 }
             }
             return 0;
