c5ff19d6dad21b6956b60aace10cb4726ddedd6b
KingPrimes <15066650082@163.com>
2023-03-02 09:56:38 +0800

    增加 黑名单配置

---
+++ zkb-server/src/main/java/com/zkb/bot/domain/BotBlack.java
@@ -0,0 +1,52 @@
+package com.zkb.bot.domain;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+@Entity
+@Table(name = "bot_black", uniqueConstraints = @UniqueConstraint(name = "botAdmins", columnNames = {"botUid", "botAdminUid"}))
+public class Black {
+    @Id
+    Long blackId;
+
+    Long userId;
+    Long groupId;
+
+    public Long getBlackId() {
+        return blackId;
+    }
+
+    public void setBlackId(Long blackId) {
+        this.blackId = blackId;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Long getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(Long groupId) {
+        this.groupId = groupId;
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this, ToStringStyle.JSON_STYLE)
+                .append("blackId", blackId)
+                .append("userId", userId)
+                .append("groupId", groupId)
+                .toString();
+    }
+}
